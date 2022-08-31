d62453895d038a3a4c73154628d7c64fc48e3f72
unknown <bennyyoga77@gmail.com>
2022-08-31 21:28:42 +0700

    Tugas Praktikum 2

---
+++ Pertemuan 1 (Intro)/Latihan2/Wellcome.java
@@ -0,0 +1,35 @@
+public class Wellcome
+{
+	public static void main (String[] args)
+	{
+		byte angka1 = 125;
+		byte angka2 = 2;
+		byte hasil = (byte) (angka1+angka2);
+
+		System.out.println("125 + 2 = " + hasil);
+
+		angka1 = 125;
+		angka2 = 3;
+		hasil = (byte) (angka1+angka2);
+
+		System.out.println("125 + 3 =" + hasil);
+
+		angka1 = 125;
+		angka2 = 4;
+		hasil = (byte) (angka1+angka2);
+
+		System.out.println("125 + 4 = " + hasil);
+
+		angka1 = 125;
+		angka2 = 5;
+		hasil = (byte) (angka1+angka2);
+
+		System.out.println("125 + 5 = " + hasil);
+
+		angka1 = 125;
+		angka2 = (byte) 200;
+		hasil = (byte) (angka1+angka2);
+
+		System.out.println("125 + 200 = " + hasil);
+	}
+}
