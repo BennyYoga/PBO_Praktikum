6fae6d3ac2961e801e996ed380fd83f81c16336b
unknown <bennyyoga77@gmail.com>
2022-10-02 18:22:04 +0700

    Inheritance

---
+++ Pertemuan 5/Tugas/Pertemuan5/src/Kasus2/Produk.java
@@ -0,0 +1,47 @@
+package Kasus2;
+
+public class Produk {
+	private String namaProduk;
+	private int harga;
+	private int stok;
+	private int id;
+	private static int nextid =0;
+	
+	{
+		this.id = nextid;
+		nextid ++;
+	}
+	
+	public String getNamaProduk() {
+		return namaProduk;
+	}
+	public void setNamaProduk(String namaProduk) {
+		this.namaProduk = namaProduk;
+	}
+	public int getHarga() {
+		return harga;
+	}
+	public void setHarga(int harga) {
+		this.harga = harga;
+	}
+	public int getStok() {
+		return stok;
+	}
+	public void setStok(int stok) {
+		this.stok = stok;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	public Produk(String nama, int harga, int stok) {
+		this.namaProduk = nama;
+		this.stok = stok;
+		this.harga = harga;
+	}
+	
+	public void print() {
+		System.out.println((this.id+1) + " " + this.namaProduk + 
+				" = Rp." + this.harga + " (" + this.stok +")");
+	}
+}
